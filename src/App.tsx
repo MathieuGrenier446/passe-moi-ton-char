3c0c1ec7c0aac5efb23559d95f5e136e4bd621cf
Mathieu Grenier <mathieuloco@gmail.com>
2025-04-30 13:49:46 -0400

    added links

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import FacebookGroupPosts from './components/FacebookGroupPosts'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfService from './pages/TermsOfService'
+import Home from './pages/Home'
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FacebookGroupPosts appId='1806425476600064' groupId='639037939134393'/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/facebook" element={<FacebookGroupPosts appId='1806425476600064' groupId='639037939134393'/>}/>
         <Route path="/privacy-policy" element={<PrivacyPolicy/>}/>
         <Route path="/terms-of-service" element={<TermsOfService/>}/>
       </Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router";
+
+export default function Home(){
+    return (
+        <nav>
+          <NavLink to="/" end>Home</NavLink>
+          <NavLink to="/privacy-policy">Privacy Policy</NavLink>
+          <NavLink to="/terms-of-service">Terms of service</NavLink>
+          <NavLink to="/facebook">Facebook</NavLink>
+        </nav>
+      );
+}
