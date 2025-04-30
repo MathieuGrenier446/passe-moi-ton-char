c3713bfb4b5cd3c45d184680816a79046de3d0ca
Mathieu Grenier <mathieuloco@gmail.com>
2025-04-30 17:48:18 -0400

    removed unused policies

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './App.css'
 import FacebookGroupPosts from './components/FacebookGroupPosts'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import TermsOfService from './pages/TermsOfService'
 import Home from './pages/Home'
 
 function App() {
-  
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/facebook" element={<FacebookGroupPosts appId='1806425476600064' groupId='639037939134393'/>}/>
-        <Route path="/privacy-policy" element={<PrivacyPolicy/>}/>
-        <Route path="/terms-of-service" element={<TermsOfService/>}/>
       </Routes>
     </BrowserRouter>
     
